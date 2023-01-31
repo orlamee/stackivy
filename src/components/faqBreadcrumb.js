6d8cc54d1bbe6301d78ba65a7ac2ec0c4e7acec6
orlamee <meetmichaelakindele@gmail.com>
2023-01-31 15:18:19 +0100

    FAQ

--- src/components/faqBreadcrumb.js
+++ src/components/faqBreadcrumb.js
@@ -4,10 +4,14 @@ function FAQBreadcrumb() {
   return (
     <section className="termsbreadcrumb">
       <div className="container">
-        <div className="row">
-          <div className="col text-center">
+        <div className="row justify-content-center">
+          <div className="col-md-10 text-center">
             <h2>Frequently Asked Questions (FAQS)</h2>
             <h6 className="my-4">Find answers to our most frequently asked questions. Can’t find the answer you're looking for? Email our member support team help@Ardilla.com</h6>
+            <div class="mt-5 row justify-content-center">
+             <input type="text" class="form-control rounded-pill" id="text" placeholder="Search for a question" name="searchbox"/>
+            </div>
+           
           </div>
         </div>
       </div>
--- src/components/faqContent.js
+++ src/components/faqContent.js
@@ -5,7 +5,159 @@ function FAQContent() {
     <section className="termscontent bg-white">
       <div className="container">
         <div className="row">
-          <h4>Loans</h4>
+          <div class="m-3 log1">
+            <div class="accordion" id="myAccordion">
+             <h2>Loans</h2>
+             <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                  <button type="button" class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapseOne">What are your Loan interest rates?</button>									
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                  <div class="card-body stack">
+                    <p>The interest rate on our loan facilities varies from bank to bank. Some factors contribute to the interest rate. Please get in touch with our customer service for more details.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>       
+          </div>
+
+          <div class="m-3 log1">
+            <div class="accordion" id="myAccordion">
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTwo">
+                  <button type="button" class="accordion-button collapsed " data-bs-toggle="collapse" data-bs-target="#collapseTwo">What is the expected ROI on Investment with Stackivy?</button>									
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                  <div class="card-body">
+                    <p>At Stackivy, we offer excellent ROI on your investment. For more details, contact us directly. Our team will be happy to help.</p>
+                  </div>
+                </div>
+              </div>
+            </div>       
+          </div>
+
+          <div class="m-3 log1">
+            <div class="accordion" id="myAccordion">
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingThree">
+                  <button type="button" class="accordion-button collapsed " data-bs-toggle="collapse" data-bs-target="#collapseThree">What banks do you work with?</button>									
+                </h2>
+                <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                  <div class="card-body">
+                    <p> We partner with Globus bank, and we also work with other top commercial banks.</p>
+                  </div>
+                </div>
+              </div>
+            </div>       
+          </div>
+          
+          <div class="m-3 log1">
+            <div class="accordion" id="myAccordion">
+              <div class="accordion-item">
+                <h2>
+                <button type="button" class="accordion-button collapsed " data-bs-toggle="collapse" data-bs-target="#collapseFour">How long does it take to fund an account?</button>
+                </h2>
+                <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                  <div class="card-body">
+                   <p>Funding your account is lightning-fast and happens within 24-48 hours of submitting your application.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="m-3 log1">
+            <div class="accordion" id="myAccordion">
+              <h2>Investments</h2>
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingFour">
+                  <button type="button" class="accordion-button collapsed " data-bs-toggle="collapse" data-bs-target="#collapseFive">What is Ardilla?</button>									
+                </h2>
+                <div id="collapseFive" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                  <div class="card-body">
+                    <p> Lorem Ipsum</p>
+                  </div>
+                </div>
+              </div>
+            </div>       
+          </div>
+
+          <div class="m-3 log1">
+            <div class="accordion" id="myAccordion"> 
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingSix">
+                 <button type="button" class="accordion-button collapsed " data-bs-toggle="collapse" data-bs-target="#collapseSix">What is Ardilla?</button>									
+                </h2>
+                <div id="collapseSix" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                  <div class="card-body">
+                    <p> Lorem ipsum</p>
+                  </div>
+                </div>
+              </div>
+            </div>       
+          </div>
+
+          <div class="m-3 log1">
+            <div class="accordion" id="myAccordion">
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingSeven">
+                  <button type="button" class="accordion-button collapsed " data-bs-toggle="collapse" data-bs-target="#collapseSeven">What is Ardilla?</button>									
+                </h2>
+                <div id="collapseSeven" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                  <div class="card-body">
+                    <p> Lorem ipsum</p>
+                  </div>
+                </div>
+              </div>
+            </div>       
+          </div>
+
+          <div class="m-3 log1">
+            <div class="accordion" id="myAccordion">
+              <h2>Savings</h2>
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingEight">
+                  <button type="button" class="accordion-button collapsed " data-bs-toggle="collapse" data-bs-target="#collapseEight">What is Ardilla?</button>									
+                </h2>
+                <div id="collapseEight" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                  <div class="card-body">
+                    <p> Lorem Ipsum</p>
+                  </div>
+                </div>
+              </div>
+            </div>       
+          </div>
+
+          <div class="m-3 log1">
+            <div class="accordion" id="myAccordion">
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingNine">
+                  <button type="button" class="accordion-button collapsed " data-bs-toggle="collapse" data-bs-target="#collapseNine">What is Ardilla?</button>									
+                </h2>
+                <div id="collapseNine" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                  <div class="card-body">
+                    <p> Lorem ipsum</p>
+                  </div>
+                </div>
+              </div>
+            </div>       
+          </div>
+
+          <div class="m-3 log1">
+            <div class="accordion" id="myAccordion"> 
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTen">
+                  <button type="button" class="accordion-button collapsed " data-bs-toggle="collapse" data-bs-target="#collapseTen">What is Ardilla?</button>									
+                </h2>
+                <div id="collapseTen" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                  <div class="card-body">
+                    <p> Lorem ipsum</p>
+                  </div>
+                </div>
+              </div>
+            </div>       
+          </div>
         </div>
       </div>
     </section>
