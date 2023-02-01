46635b592d26bedd713c2968079d64a2c0c465e7
orlamee <meetmichaelakindele@gmail.com>
2023-02-01 10:31:30 +0100

    fixes

--- src/components/mission.js
+++ src/components/mission.js
@@ -5,7 +5,12 @@ function Mission() {
     <section className="mission">
       <div className="container">
         <div className="row ">
-         
+         <div className="col-md-6">
+          <div className="bg-m">
+            <h4 className="text-white mb-4">Our Mission</h4>
+            <h6 className="text-white">Building an organization with world-class skills and management capability to achieve the Company's vision.</h6>
+          </div>
+         </div>
         </div>
       </div>
     </section>
